f9e9891f78131809d4eb74e40235ae8d5fd0b941
Hugo Protsch <hugo@protsch.dev>
2022-09-29 22:36:00 +0200

    Add proof of concept for fixed update tick

--- CitySim/Program.cs
+++ CitySim/Program.cs
@@ -5,10 +5,13 @@ using Mars.Interfaces.Model;
 using NetTopologySuite.Algorithm;
 using Raylib_CsLo;
 using System.Numerics;
+using Mars.Core.Simulation;
+using Mars.Interfaces.Layers;
 using static Raylib_CsLo.Raylib;
 
 var desc = new ModelDescription();
 desc.AddLayer<GridLayer>();
+desc.AddLayer<FixedUpdateLayer>();
 desc.AddAgent<Person, GridLayer>();
 
 var config = new SimulationConfig
@@ -25,13 +28,15 @@ var config = new SimulationConfig
     }
 };
 
-var task = SimulationStarter.Start(desc, config);
-var loopResults = task.Run();
-// Feedback to user that simulation run was successful
-Console.WriteLine($"Simulation execution finished after {loopResults.Iterations} steps");
+var application = SimulationStarter.BuildApplication(desc, config);
+var simulation = application.Resolve<ISimulation>();
+Task.Factory.StartNew(() =>
+{
+    var loopResults = simulation.StartSimulation();
+    Console.WriteLine($"Simulation execution finished after {loopResults.Iterations} steps");
+});
 
-
-
+GridLayer gridLayer = (GridLayer)simulation.WorkflowState.Model.Layers[new LayerType(typeof(GridLayer))];
 
 // Initialization
 //--------------------------------------------------------------------------------------
@@ -47,26 +52,17 @@ while (!WindowShouldClose())
 {
     BeginDrawing();
 
-    ClearBackground(new Color(10,130,255,255));
+    ClearBackground(new Color(10, 130, 255, 255));
 
 
-    for (int x = 0; x < 100; x++)
+    for (int x = 0; x < 10; x++)
     {
-        for (int y = 0; y < 100; y++)
+        for (int y = 0; y < 10; y++)
         {
-            if(Vector2.Distance(new Vector2(x,y), new Vector2(8, 6))<5)
-                DrawRectangleRounded(new Rectangle(x * 50-2, y * 30-2, 50+4, 30 + 4), 0.2f, 3,
-                    new Color(30, 80, 0, 255));
-        }
-    }
-
-    for (int x = 0; x < 100; x++)
-    {
-        for (int y = 0; y < 100; y++)
-        {
-            if (Vector2.Distance(new Vector2(x, y), new Vector2(8, 6)) < 5)
-                DrawRectangleRounded(new Rectangle(x * 50 + 1, y * 30 + 1, 50 - 2, 30 - 2), 0.1f, 3,
-                    new Color(50, 255, 0, 255));
+            var personOnCoord = gridLayer.GridEnvironment.Explore(x, y, 0).Any();
+            Color color = personOnCoord ? new Color(50, 255, 0, 255) : new Color(50, 255,255,255);
+            DrawRectangleRounded(new Rectangle(x * 50 + 150, y * 30 + 100, 50 - 2, 30 - 2), 0.1f, 3,
+                    color);
         }
     }
 
---
+++ CitySim/World/FixedUpdateLayer.cs
@@ -0,0 +1,21 @@
+using Mars.Components.Layers;
+using Mars.Interfaces.Layers;
+
+namespace CitySim.World;
+
+public class FixedUpdateLayer : AbstractLayer, ISteppedActiveLayer
+{
+    public void Tick()
+    {
+        Task.Delay(TimeSpan.FromMilliseconds(1000)).Wait();
+        //todo
+    }
+
+    public void PreTick()
+    {
+    }
+
+    public void PostTick()
+    {
+    }
+}

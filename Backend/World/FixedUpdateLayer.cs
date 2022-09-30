4bdb5224d1b4b8536a96734a63ebd610960774ab
Hugo Protsch <hugo@protsch.dev>
2022-09-30 12:28:52 +0200

    Add some debug output

--- Backend/World/FixedUpdateLayer.cs
+++ Backend/World/FixedUpdateLayer.cs
@@ -8,15 +8,21 @@ public class FixedUpdateLayer : AbstractLayer, ISteppedActiveLayer
     internal SimulationController SimulationController = null!; // set in CitySim ctor
     public void Tick()
     {
-        Task.Delay(TimeSpan.FromMilliseconds(1000 / SimulationController.TimeMultiplier)).Wait();
-        //todo
     }
 
     public void PreTick()
     {
+        Console.WriteLine($"### Starting Tick: {Context.CurrentTick} ###");
     }
 
     public void PostTick()
     {
+        Console.WriteLine($"### Finished Tick: {Context.CurrentTick} ###");
+        Console.WriteLine($"Current simulation speed multiplier: {SimulationController.TimeMultiplier:F1}");
+        
+        //todo calculate
+        var timeToWait = 1000 / SimulationController.TimeMultiplier;
+        Console.WriteLine($"Waiting: {timeToWait:F1}ms");
+        Task.Delay(TimeSpan.FromMilliseconds(timeToWait)).Wait();
     }
 }

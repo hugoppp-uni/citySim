957e107475127481b8559cdfe361f9ac9b356441
jupahe64 <jupahe@icloud.com>
2022-10-31 16:25:09 +0100

    refactor WorldDrawer

--- Frontend/WorldDrawer.cs
+++ Frontend/WorldDrawer.cs
@@ -45,12 +45,24 @@ namespace CitySim.Frontend
             HOUSING_SCORE
         }
 
+        private const int TILE_BUILDING_FLOOR = 0;
+        private const int TILE_BUILDING_GROUND = 1;
+        private const int TILE_RESTAURANT_BOTTOMLEFT = 4;
+        private const int TILE_BUILDING_ROOF = 5;
+        private const int TILE_GRASS_GROUND = 67;
+
         private readonly Backend.CitySim _model;
         private readonly SpriteSheet _terrainSheet;
         private readonly SpriteSheet _buildingSheet;
         private static readonly Dictionary<byte, int> s_roadMap = new Dictionary<byte, int>()
         {
-            //connections bitfield: Y, X, -Y, -X
+            //connections bitfield: topLeft, topRight, bottomRight, bottomLeft
+
+            //<0>     <1>
+            //    < >
+            //<3>     <2>
+
+            //b_0123, TILE_ID
             {0b_1010, 74},
             {0b_0101, 82},
             {0b_1111, 90},
@@ -67,6 +79,23 @@ namespace CitySim.Frontend
             {0b_1001, 126},
             {0b_1100, 127},
         };
+
+        private static bool TryGetRoadConnectionTile(bool topLeft, bool topRight, bool bottomRight, bool bottomLeft, out int tile)
+        {
+            byte bitField = 0;
+
+            if (topLeft)
+                bitField |= 0b1000;
+            if (topRight)
+                bitField |= 0b0100;
+            if (bottomRight)
+                bitField |= 0b0010;
+            if (bottomLeft)
+                bitField |= 0b0001;
+
+            return s_roadMap.TryGetValue(bitField, out tile);
+        }
+
 
         private bool[] _overlaysEnabled;
 
@@ -112,111 +141,115 @@ namespace CitySim.Frontend
             return new((float)person.Position.X + xoff, (float)person.Position.Y + yoff, 0);
         }
 
+        private void DrawBuildingTile(int tile, Vector2 bottomCenter)
+        {
+            string name = $"buildingTiles_{tile:000}.png";
+            var rect = _buildingSheet.Rects[name];
+            _buildingSheet.DrawSprite(name,
+                bottomCenter - new Vector2(rect.width / 2, rect.height - 64));
+        }
+
+        private void DrawTerrainTile(int tile, Vector2 bottomCenter)
+        {
+            string name = $"landscapeTiles_{tile:000}.png";
+            var rect = _terrainSheet.Rects[name];
+            _terrainSheet.DrawSprite(name,
+                bottomCenter - new Vector2(rect.width / 2, rect.height - 64));
+        }
+
+        private static bool IsGround(int tileX, int tileY)
+        {
+            Vector2 pos = new Vector2(tileX + 0.5f, tileY + 0.5f);
+
+            return Vector2.Distance(pos, new Vector2(202, 40)) < 205;
+        }
+
+        private bool IsRoad(int tileX, int tileY) => _model.WorldLayer.Structures[tileX, tileY] is Street;
+
+        private static void DrawPerson(Person person, Vector2 position2d)
+        {
+            Vector2 pos = position2d - new Vector2(1, 1);
+
+            MyDrawRoundedRect(pos.X - 10, pos.Y - 25, pos.X + 10, pos.Y, 5, new Color(0, 0, 0, 50));
+            DrawEllipse((int)pos.X, (int)pos.Y - 40, 10, 10, new Color(0, 0, 0, 50));
+
+            pos = position2d;
+
+            int hash = person.GetHashCode();
+            Color col = ColorFromHSV((uint)hash % 360, 70, 100);
+
+            MyDrawRoundedRect(pos.X - 10, pos.Y - 25, pos.X + 10, pos.Y, 5, col);
+            DrawEllipse((int)pos.X, (int)pos.Y - 40, 10, 10, WHITE);
+        }
+
+        private void DrawBuilding(Vector2 position2d, float cell_height, int stories)
+        {
+            DrawBuildingTile(TILE_BUILDING_GROUND, position2d);
+
+            for (int i = 0; i < stories; i++)
+            {
+                DrawBuildingTile(TILE_BUILDING_FLOOR, position2d - new Vector2(0,
+                    cell_height * (2.3f + i)));
+            }
+
+            DrawBuildingTile(TILE_BUILDING_ROOF, position2d - new Vector2(0,
+                    cell_height * (2.3f + stories)));
+        }
+
         public void Draw(Camera2D camera)
         {
-            void DrawTerrainTile(int tile, Vector2 bottomCenter)
-            {
-                string name = $"landscapeTiles_{tile:000}.png";
-                var rect = _terrainSheet.Rects[name];
-                _terrainSheet.DrawSprite(name,
-                    bottomCenter - new Vector2(rect.width / 2, rect.height - 64));
-            }
 
-            void DrawBuildingTile(int tile, Vector2 bottomCenter)
-            {
-                string name = $"buildingTiles_{tile:000}.png";
-                var rect = _buildingSheet.Rects[name];
-                _buildingSheet.DrawSprite(name,
-                    bottomCenter - new Vector2(rect.width / 2, rect.height - 64));
-            }
-
-
-            bool IsGround(int tileX, int tileY)
-            {
-                Vector2 pos = new Vector2(tileX + 0.5f, tileY + 0.5f);
-
-                return Vector2.Distance(pos, new Vector2(202, 40)) < 205;
-            }
-
-            var coordsWithPerson = _model.WorldLayer.GridEnvironment.Entities.OfType<Person>()
+            var personsGroupedByCoord = _model.WorldLayer.GridEnvironment.Entities.OfType<Person>()
                 .GroupBy(p => (p.Position.X, p.Position.Y))
                 .ToDictionary(x => x.Key, x => x);
-
-            bool IsRoad(int tileX, int tileY) => _model.WorldLayer.Structures[tileX, tileY]?.GetType() == typeof(Street);
 
             foreach (var (cell_x, cell_y, position2d, cell_height) in Grid.GetVisibleCells(camera))
             {
                 if (!IsGround(cell_x, cell_y))
                     continue;
 
-                //connections bitfield: Y, X, -Y, -X
+                
 
-                byte connections = 0;
                 if (IsRoad(cell_x, cell_y))
                 {
-                    if (IsRoad(cell_x, cell_y + 1))
-                        connections |= 0b1000;
-                    if (IsRoad(cell_x + 1, cell_y))
-                        connections |= 0b0100;
-                    if (IsRoad(cell_x, cell_y - 1))
-                        connections |= 0b0010;
-                    if (IsRoad(cell_x - 1, cell_y))
-                        connections |= 0b0001;
-
-                    if (connections == 0)
+                    if(TryGetRoadConnectionTile(
+                        IsRoad(cell_x, cell_y + 1),
+                        IsRoad(cell_x + 1, cell_y),
+                        IsRoad(cell_x, cell_y - 1),
+                        IsRoad(cell_x - 1, cell_y), 
+                        out int tile))
+                    {
+                        DrawTerrainTile(tile, position2d);
+                    }
+                    else
                     {
                         DrawTerrainTile(67, position2d);
-                        continue;
                     }
-
-                    DrawTerrainTile(s_roadMap[connections], position2d);
                 }
                 else if (_model.WorldLayer.Structures[cell_x, cell_y]?.GetType() == typeof(Restaurant))
                 {
-                    DrawBuildingTile(4, position2d);
+                    DrawBuildingTile(TILE_RESTAURANT_BOTTOMLEFT, position2d);
                 }
                 else if (_model.WorldLayer.Structures[cell_x, cell_y]?.GetType() == typeof(House))
                 {
-                    DrawBuildingTile(1, position2d);
-
-                    int height = (int)Math.Round(3 + Math.Sin(cell_x + 3) + Math.Sin(cell_y + 3));
-
-                    for (int i = 0; i < height; i++)
-                    {
-                        DrawBuildingTile(0, position2d - new Vector2(0,
-                            cell_height * (2.3f + i)));
-                    }
-
-                    DrawBuildingTile(5, position2d - new Vector2(0,
-                            cell_height * (2.3f + height)));
+                    int stories = (int)Math.Round(3 + Math.Sin(cell_x + 3) + Math.Sin(cell_y + 3));
+                    DrawBuilding(position2d, cell_height, stories);
                 }
                 else
                 {
-                    DrawTerrainTile(67, position2d);
+                    DrawTerrainTile(TILE_GRASS_GROUND, position2d);
                 }
 
 
                 //Draw persons
-                if (coordsWithPerson.TryGetValue((cell_x, cell_y), out IGrouping<(double, double), Person>? persons))
+                if (personsGroupedByCoord.TryGetValue((cell_x, cell_y), out IGrouping<(double, double), Person>? persons))
                 {
                     foreach (var person in persons)
                     {
                         Vector2 _position2d = Grid.GetPosition2D(GetPersonPosition(person));
-
-                        Vector2 pos = _position2d - new Vector2(1, 1);
-
-                        MyDrawRoundedRect(pos.X - 10, pos.Y - 25, pos.X + 10, pos.Y, 5, new Color(0, 0, 0, 50));
-                        DrawEllipse((int)pos.X, (int)pos.Y - 40, 10, 10, new Color(0, 0, 0, 50));
-
-                        pos = _position2d;
-
-                        int hash = person.GetHashCode();
-                        Color col = ColorFromHSV((uint)hash % 360, 70, 100);
-
-                        MyDrawRoundedRect(pos.X - 10, pos.Y - 25, pos.X + 10, pos.Y, 5, col);
-                        DrawEllipse((int)pos.X, (int)pos.Y - 40, 10, 10, WHITE);
+                        DrawPerson(person, _position2d);
                     }
-                    
+
                 }
             }
 
@@ -242,7 +275,7 @@ namespace CitySim.Frontend
 
             if (_overlaysEnabled[(int)Overlay.PERSON_COUNT])
             {
-                foreach (var (coord, persons) in coordsWithPerson)
+                foreach (var (coord, persons) in personsGroupedByCoord)
                 {
                     Vector2 pos = Grid.GetPosition2D(new Vector3((float)coord.X, (float)coord.Y, 0));
 

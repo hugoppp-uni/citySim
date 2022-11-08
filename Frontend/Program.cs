e8a7860f10398d4021255901d81a79f9442f42df
jupahe64 <jupahe@icloud.com>
2022-11-08 17:58:48 +0100

    add transition from loading screen to game view and rework the code a bit

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -1,6 +1,7 @@
 ﻿using CitySim.Frontend;
 using Raylib_CsLo;
 using System.Numerics;
+using System.Security.Policy;
 using static Raylib_CsLo.Raylib;
 
 Console.WriteLine("Hello, World!");
@@ -27,8 +28,9 @@ var citySim = new CitySim.Backend.CitySim
         TicksPerSecond = 2
     }
 };
-var simulationTask = citySim.StartAsync();
 
+Task simulationTask;
+bool isSimulationRunning = false;
 
 
 SetTargetFPS(60);
@@ -40,30 +42,40 @@ RayGui.GuiSetStyle((int)RaylibExtensions.GuiControl.DEFAULT, (int)RaylibExtensio
 
 Camera2D cam = new Camera2D()
 {
-    zoom = 1
+    zoom = 4
 };
+
+
+Vector2 HeadPosition = new Vector2(0, -40);
+
+float HeadRadius = 10;
+
+float BodyLeft = -10;
+float BodyRight = 10;
+float BodyTop = -25;
+float BodyBottom = 0;
+
+float BodyCornerRadius = 5;
+
+
+
 
 int Circle_VertexCount = 128;
 
 Vector2[] headVerts = new Vector2[Circle_VertexCount + 2];
 Vector2[] headVertsDynamic = new Vector2[Circle_VertexCount + 2];
 
-headVerts[0] = new Vector2(0, -100);
+headVerts[0] = HeadPosition;
 
 for (int i = 0; i < Circle_VertexCount + 1; i++)
 {
     float a = MathF.PI * 2 / Circle_VertexCount * i;
     var (sin, cos) = MathF.SinCos(a);
-    headVerts[i + 1] = headVerts[0] + new Vector2(sin, cos) * 50;
+    headVerts[i + 1] = HeadPosition + new Vector2(sin, cos) * HeadRadius;
 }
 
 
-float RowHeight = 2f;
-
-float BodyTop = -40;
-float BodyBottom = 40;
-
-float BodyCornerRadius = 10;
+float RowHeight = 0.5f;
 
 int Rows = (int)((BodyBottom - BodyTop) / RowHeight);
 
@@ -85,8 +97,8 @@ for (int i = 0; i < Rows+1; i++)
 
     float d = 1-MathF.Sqrt(1 - t * t);
 
-    bodyVerts[i * 2] = new Vector2(50 - d * BodyCornerRadius, y);
-    bodyVerts[i * 2 + 1] = new Vector2(-50 + d * BodyCornerRadius, y);
+    bodyVerts[i * 2] = new Vector2(BodyRight - d * BodyCornerRadius, y);
+    bodyVerts[i * 2 + 1] = new Vector2(BodyLeft + d * BodyCornerRadius, y);
 
 }
 
@@ -94,11 +106,6 @@ for (int i = 0; i < Rows+1; i++)
 while (!WindowShouldClose())
 {
     BeginDrawing();
-    ClearBackground(BLACK);
-    //view.UpdateAndDraw(GetScreenWidth(), GetScreenHeight());
-
-    cam.offset = new Vector2(GetScreenWidth(), GetScreenHeight()) / 2;
-
 
     static float EaseOutElastic(float x)
     {
@@ -120,110 +127,146 @@ while (!WindowShouldClose())
     float jumpProgress = Math.Clamp((float)time - 1.6f, 0, 0.4f) / 0.4f;
 
     float textFade = Math.Clamp((float)time - 1.8f, 0, 0.6f) / 0.6f;
+    
+    float finalWipe = Math.Clamp((float)time - 2.4f, 0, 0.8f) / 0.8f;
+
+
+    if (finalWipe == 1 && !isSimulationRunning)
+    {
+        citySim.StartAsync();
+        isSimulationRunning = true;
+    }
+
+    if (finalWipe > 0)
+        view.UpdateAndDraw(GetScreenWidth(), GetScreenHeight());
+
+    cam.offset = new Vector2(GetScreenWidth(), GetScreenHeight()) / 2;
+
+
 
 
     float splitAnim = EaseOutElastic(splitProgress);
 
+    float t3 = jumpProgress;
+
+    float y_offset = (-(t3 * t3) + 2 * t3) * -15;
+
+    Vector2 posA = Vector2.Lerp(
+        new Vector2(0, 10),
+        new Vector2(-20, 10 + y_offset),
+        splitAnim);
+
+    Vector2 posB = Vector2.Lerp(
+        new Vector2(0, 10),
+        new Vector2(20, 10 + y_offset),
+        splitAnim);
+
+    BeginScissorMode(0, 0, (int)(GetScreenWidth() * (1 - finalWipe * finalWipe * finalWipe)), GetScreenHeight());
+    ClearBackground(BLACK);
     BeginMode2D(cam);
 
-    unsafe
+
+
+    //Fading in CitySim text
     {
-        Vector2 Func(float y)
-        {
-            float headTop = -150;
-
-            float t_y = (y - BodyBottom) / (BodyBottom - headTop);
-
-            float t1 = pealProgress;
-            float t2 = splitAnim;
-            float t3 = jumpProgress;
-
-            float t = (float)time;
-
-
-
-            float a = t_y * t_y * t1 * (50 + MathF.Sin(t * 50) * 4);
-            float b = 0;
-
-            return new Vector2(
-                a * (1 - t2) + b * t2,
-                (-(t3 * t3) + 2 * t3) * -30);
-        }
-
-
-        Vector2 posA = Vector2.Lerp(
-            new Vector2(0, 0),
-            new Vector2(0, 0),
-            splitAnim);
-
-        Vector2 posB = Vector2.Lerp(
-            new Vector2(0, 0),
-            new Vector2(120, 160),
-            splitAnim);
-
-
-
         Font font = GetFontDefault();
 
-        float fontSize = 60;
+        float fontSize = 30;
 
         var size = MeasureTextEx(font, "CitySim", fontSize, fontSize / font.baseSize);
 
         var col = ColorAlpha(WHITE, textFade);
 
-        DrawText("CitySim", 0 - size.X / 2, 60 - size.Y / 2, fontSize, col);
+        DrawText("CitySim", 0 - size.X / 2, 20 - size.Y / 2, fontSize, col);
+    }
+
+    //Cell dividing (splitting) bodies
+    unsafe
+    {
+        Color headColorA = WHITE;
+        Color headColorB = WHITE;
+
+        Color bodyColorA = WHITE;
+        Color bodyColorB = WHITE;
+
+        Color _bodyColorB = splitProgress > 0 ? bodyColorB : bodyColorA;
+
+
+
+        Vector2 VertexTransformation(Vector2 pos)
+        {
+            float headTop = -150;
+
+            float t_y = (pos.Y - BodyBottom) / (BodyBottom - headTop);
+
+            float t1 = pealProgress;
+            float t2 = splitAnim;
+
+
+            float t = (float)time;
+            float a = t_y * t_y * t1 * (50 + MathF.Sin(t * 50) * 4);
+            float b = 0;
+
+            return pos + new Vector2(
+                a * (1 - t2) + b * t2,
+                0);
+        }
+
+        Vector2 mirrorX = new Vector2(-1, 1);
+
 
         for (int i = 0; i < headVertsDynamic.Length; i++)
         {
-            headVertsDynamic[i] = headVerts[i] +
-                Func(headVerts[i].Y) * new Vector2(-1, 1) + posA;
+            headVertsDynamic[i] =
+                VertexTransformation(headVerts[i] * mirrorX) * mirrorX + posA;
         }
 
         fixed (Vector2* hvp = headVertsDynamic)
         {
-            DrawTriangleFan(hvp, headVerts.Length, WHITE);
+            DrawTriangleFan(hvp, headVerts.Length, headColorA);
         }
 
 
         for (int i = 0; i < headVertsDynamic.Length; i++)
         {
-            headVertsDynamic[i] = headVerts[i] +
-                Func(headVerts[i].Y) * new Vector2(1, 1) + posB;
+            headVertsDynamic[i] =
+                VertexTransformation(headVerts[i]) + posB;
         }
 
         fixed (Vector2* hvp = headVertsDynamic)
         {
-            DrawTriangleFan(hvp, headVerts.Length, WHITE);
+            DrawTriangleFan(hvp, headVerts.Length, headColorB);
         }
 
 
 
         for (int i = 0; i < bodyVertsDynamic.Length; i++)
         {
-            bodyVertsDynamic[i] = bodyVerts[i] +
-                Func(bodyVerts[i].Y) * new Vector2(-1, 1) + posA;
+            bodyVertsDynamic[i] = 
+                VertexTransformation(bodyVerts[i] * mirrorX) * mirrorX + posA;
         }
 
         fixed (Vector2* bvp = bodyVertsDynamic)
         {
-            DrawTriangleStrip(bvp, bodyVerts.Length, WHITE);
+            DrawTriangleStrip(bvp, bodyVerts.Length, bodyColorA);
         }
 
 
 
         for (int i = 0; i < bodyVertsDynamic.Length; i++)
         {
-            bodyVertsDynamic[i] = bodyVerts[i] +
-                Func(bodyVerts[i].Y) * new Vector2(1, 1) + posB;
+            bodyVertsDynamic[i] =
+                VertexTransformation(bodyVerts[i]) + posB;
         }
 
         fixed (Vector2* bvp = bodyVertsDynamic)
         {
-            DrawTriangleStrip(bvp, bodyVerts.Length, WHITE);
+            DrawTriangleStrip(bvp, bodyVerts.Length, _bodyColorB);
         }
     }
 
     EndMode2D();
+    EndScissorMode();
 
     EndDrawing();
 }
